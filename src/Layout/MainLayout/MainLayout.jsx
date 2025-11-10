77a8519b33861230f69d92841da277864a0f18b3
ahmednabelqteb <nabelahmed102@gmail.com>
2025-11-10 11:43:20 +0200

    finished fully resposive page + added scrollToTop

--- src/Layout/MainLayout/MainLayout.jsx
+++ src/Layout/MainLayout/MainLayout.jsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import { Outlet } from "react-router-dom";
 import { Container } from "@/components/ui/container";
+import { ScrollToTop } from "@/components/ScrollToTop";
 
 const MainLayout = () => {
   const [headerHeight, setHeaderHeight] = useState(0);
@@ -11,16 +12,15 @@ const MainLayout = () => {
     <div className="flex flex-col min-h-screen">
       {/* Fixed Header */}
       <Header onHeightChange={setHeaderHeight} />
-
       {/* Main Content (fills remaining space) */}
       <main className="flex-1" style={{ paddingTop: `${headerHeight}px` }}>
         <Container>
           <Outlet />
         </Container>
       </main>
-
       {/* Footer always at bottom */}
       <Footer />
+      <ScrollToTop /> {/* Global scroll to top button */}
     </div>
   );
 };
---
+++ src/components/CareerResources/CareerResources.jsx
@@ -0,0 +1,49 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import { MapPin, Clock, Briefcase } from "lucide-react"; // optional icons
+
+import resources from "@/data/careerResources.json";
+
+export const CareerResources = () => {
+  return (
+    <section className="py-12">
+      <h2 className="text-2xl font-bold mb-8 text-center">
+        Empowering Career Resources
+      </h2>
+
+      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 ">
+        {resources.map((career) => (
+          <Card key={career.id} className="flex flex-col">
+            <CardHeader className="flex items-center gap-4 py-6">
+              <img
+                src={career.image}
+                className="w-12 h-12 rounded-md object-cover flex-shrink-0"
+              />
+              <div>
+                <CardTitle>{career.title}</CardTitle>
+              </div>
+            </CardHeader>
+
+            <CardContent className="flex flex-col flex-grow">
+              <p className="text-sm text-muted-foreground flex-grow">
+                {career.description}
+              </p>
+            </CardContent>
+
+            <CardFooter className="pb-6 flex justify-end">
+              <Button variant="outline">{career.buttonLabel}</Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </section>
+  );
+};
